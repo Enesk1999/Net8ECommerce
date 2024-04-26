3faca618f84b73434611cd70773131b5ec15e8c9
Enesk1999 <130606254+Enesk1999@users.noreply.github.com>
2024-04-26 20:41:40 +0300

    Upload Last Upgrades as styles,areas

--- ETicaret.Net8/Areas/Admin/Controllers/CategoryController.cs
+++ ETicaret.Net8/Areas/Admin/Controllers/CategoryController.cs
@@ -4,8 +4,9 @@ using ETicaret.Net8.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace ETicaret.Net8.Controllers
+namespace ETicaret.Net8.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         //private readonly ApplicationDbContext dbContext; UnitofWorke geçiş
@@ -16,7 +17,7 @@ namespace ETicaret.Net8.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var getAll= await categoryRepository.GetAllAsync();
+            var getAll = await categoryRepository.GetAllAsync();
             return View(getAll);
         }
 
@@ -28,7 +29,7 @@ namespace ETicaret.Net8.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(Category? category)
         {
-            if(category.Name == category.DisplayOrder.ToString())       //Eğer ki Görüntülenme sayısı ile kategori adı aynı ise 
+            if (category.Name == category.DisplayOrder.ToString())       //Eğer ki Görüntülenme sayısı ile kategori adı aynı ise 
             {
                 ModelState.AddModelError("Name", "Kategori Adı ile Görüntülenme Sayısı Aynı Olamaz!!!");
             }
@@ -37,7 +38,7 @@ namespace ETicaret.Net8.Controllers
             {
                 if (category != null)
                 {
-                 
+
                     await categoryRepository.AddAsync(category);
                     categoryRepository.Save();
                     TempData["basarili"] = category.Name + " " + "Başarıyla Eklendi";
@@ -51,18 +52,18 @@ namespace ETicaret.Net8.Controllers
             }
 
             return View();
-           
+
         }
         [HttpGet]
         public async Task<IActionResult> Edit(int id)
-        { 
-            if(id==null || id == 0)
+        {
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
             // vargetById = await dbContext.Categories.Where(x => x.Id == id).FirstOrDefaultAsync();    1.Metod
             // var getById = await dbContext.Categories.FindAsync(id);                                  2.Metod
-            var getById = await categoryRepository.GetAsync(x=>x.Id==id);
+            var getById = await categoryRepository.GetAsync(x => x.Id == id);
             return View(getById);
         }
 
@@ -71,13 +72,13 @@ namespace ETicaret.Net8.Controllers
         {
             if (category.Name == category.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("Name","Kategori Adı ile Görüntülenme Sırası Aynı Olamaz");
+                ModelState.AddModelError("Name", "Kategori Adı ile Görüntülenme Sırası Aynı Olamaz");
             }
             if (ModelState.IsValid)
             {
-                if(category != null)
+                if (category != null)
                 {
-                    
+
                     categoryRepository.Update(category);
                     categoryRepository.Save();
                     TempData["basarili"] = category.Name + " " + "Başarıyla Güncellendi";
@@ -87,7 +88,7 @@ namespace ETicaret.Net8.Controllers
             else
             {
                 ModelState.AddModelError("", "Bir Hata Oluştu");
-                
+
             }
             return View(category);
         }
@@ -95,22 +96,22 @@ namespace ETicaret.Net8.Controllers
         [HttpGet]
         public async Task<IActionResult> Delete(int id)
         {
-            if(id==null || id == 0)
+            if (id == null || id == 0)
             {
                 return BadRequest();
             }
-            var getByWillDeleteId = await categoryRepository.GetAsync(x=>x.Id==id);
+            var getByWillDeleteId = await categoryRepository.GetAsync(x => x.Id == id);
             return View(getByWillDeleteId);
         }
         [HttpPost]
         public IActionResult Delete(Category category)      //model binding ile categorydeki elemanları görme
         {
-            if(category != null)
+            if (category != null)
             {
                 TempData["basarili"] = "Başarıyla Kaldırıldı";
                 categoryRepository.Remove(category);
                 categoryRepository.Save();
-               
+
                 return RedirectToAction("Index");
             }
             return View(category);
---
+++ ETicaret.Net8/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaret.Net8.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
